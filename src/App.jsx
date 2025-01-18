129c9a4ed92d8e220dc01d86f63b41431d4d326e
sunny080 <sunnyrajputtrana00@gmail.com>
2025-01-19 02:30:12 +0500

    feat: init product page

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,15 @@ import "slick-carousel/slick/slick-theme.css";
 import "slick-carousel/slick/slick.css";
 import Home from './pages/Home'
 function App() {
+  const ProductPage = lazy(() => import("./pages/ProductPage"));
+
   return (
     <>
       <BrowserRouter>
         <Suspense fallback={null}>
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/product-page/:id" element={<ProductPage />} />
           </Routes>
         </Suspense>
       </BrowserRouter>
